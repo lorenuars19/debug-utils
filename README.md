439f6d6456301f3a654983424a56bef474469e76
lorenuar s19 <lorenuar.s19@gmail.com>
2021-04-01 23:53:44 +0200

    added new macro and refactored some of the code

--- README.md
+++ README.md
@@ -6,21 +6,23 @@ Header file containing useful debug macros I use all the time
 
 # Macros
 Automatic Macros
-- `BR` : BreakPoint : will pause the execution using libc `getchar()` so you can just press enter in the terminal to continue your program, you can `-D BREAK_PAUSE=0` so it does not pause your program
-- `BM(msg)` : Same as `BR` but with a custom message
-- `DE(var)` : Will try to detect variable type, and print it accordingly, using `_Generic()`
+- `BR` : BreakPoint : will pause the execution using libc `getchar()` so you can just press enter in the terminal to continue your program, you can `-D BREAK_PAUSE=0` so it does not pause your program.
+- `BM(msg)` : Same as `BR` but with a custom message.
+- `DE(var)` : Will try to detect variable type, and print it accordingly, using `_Generic()`.
+- `DM(msg, var)` : same as `DE` but you can put a message.
 
 Static typed (in case `_Generic()` fails)
-- `D_INT(var)` : (int) Print variable and its value
-- `D_LINT(var)` : (long) Print variable and its value
-- `D_STR(var)` : (String) Print variable and its value
-- `D_PTR(var)` : (Pointer) Print variable and its value
+- `D_INT(var)` : (int) Print variable and its value.
+- `D_LINT(var)` : (long) Print variable and its value.
+- `D_STR(var)` : (String) Print variable and its value.
+- `D_PTR(var)` : (Pointer) Print variable and its value.
 
 To debug memory manipulation with strings
 - `D_STR_DETAILS(str)` : Prints a string in details, printing control chars by their numeric value in the ascii table.
 
 # Contributing
-Feel free to make a pull request to add your own macros
+Feel free to make a pull request to add your own macros.
 
 # Thanks
 Thanks to [Jjosephi](https://github.com/FlavorlessQuark), for teaching me the existence of `_Generic()`
+Thanks to [Mhadad](https://github.com/Matthew-Dreemurr), for fixes.
--- debug_utils.h
+++ debug_utils.h
@@ -6,7 +6,7 @@
 /*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 18:21:34 by lorenuar          #+#    #+#             */
-/*   Updated: 2021/03/30 06:07:04 by lorenuar         ###   ########.fr       */
+/*   Updated: 2021/04/01 23:45:26 by lorenuar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@
 # endif
 
 # define _BR(NEWLINE) printf("< %s:%d in %s() >%s", __FILE__, __LINE__, __FUNCTION__, (NEWLINE == 1) ? ("\n") : (" "));
-# define _BR_MSG(NEWLINE, msg) printf("{ " #msg " } - "); _BR(NEWLINE);
+# define _BR_MSG(msg) printf("{ " #msg " } - ");
 
 # if BREAK_PAUSE == 1
 #  define BR _BR(0) getchar();
@@ -42,21 +42,25 @@
 # endif
 
 # if _BREAK_PAUSE == 1
-#  define BM(msg) _BR_MSG(0, msg) getchar();
+#  define BM(msg) _BR_MSG(msg) _BR(0); getchar();
 # else
-#  define BM(msg) _BR_MSG(1, msg);
+#  define BM(msg) _BR_MSG(msg) _BR(1);
 # endif
 
 #define _CONV(var)
 
-# define DE(var) _BR(0); _Generic(((var)+0),	\
-	int		: printf(" |" #var " = " "%d" _DE_NL , var),		\
-	long	: printf(" |" #var " = " "%ld" _DE_NL , var),	\
-	double	: printf(" |" #var " = " "%f" _DE_NL , var),		\
-	float	: printf(" |" #var " = " "%f" _DE_NL , var),		\
-	size_t	: printf(" |" #var " = " "%lu" _DE_NL , var),	\
-	char*	: printf(" |" #var " = " "\"%s\"" _DE_NL , var),	\
-	default	: printf(" |" #var " = " "%p" _DE_NL , var));
+# define _DE_AUTO(var) _Generic(((var)+0),	\
+	int		: printf("|" #var " = " "%d" _DE_NL , var),		\
+	long	: printf("|" #var " = " "%ld" _DE_NL , var),	\
+	double	: printf("|" #var " = " "%f" _DE_NL , var),		\
+	float	: printf("|" #var " = " "%f" _DE_NL , var),		\
+	size_t	: printf("|" #var " = " "%lu" _DE_NL , var),	\
+	char*	: printf("|" #var " = " "\"%s\"" _DE_NL , var),	\
+	default	: printf("|" #var " = " "%p" _DE_NL , var));
+
+# define DE(var) _BR(0) _DE_AUTO(var);
+
+# define DM(msg, var) _BR(0) _BR_MSG(msg) _DE_AUTO(var);
 
 # define D_INT(var) printf("< %s:%d in %s() > " #var " : %d" _DE_NL, __FILE__, __LINE__, __FUNCTION__, var);
 # define D_LINT(var) printf("< %s:%d in %s() > " #var " : %ld" _DE_NL, __FILE__, __LINE__, __FUNCTION__, var);
