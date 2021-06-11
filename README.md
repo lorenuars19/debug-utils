81f4bc298cbfa909608b16d50c9d6f79ea88059a
lorenuar.s19 <lorenuar.s19@gmail.com>
2021-06-11 21:07:07 +0200

    Add recursive LINE builder + update README + add backup of article that helped me a lot

--- README.md
+++ README.md
@@ -8,30 +8,42 @@ sudo cp debug_utils.h /usr/include/debug_utils.h
 # Debug-Utils
 Header file containing useful debug macros I use all the time
 
-# Parameters
-- `BREAK_PAUSE` (default : 1) compile with `-D BREAK_PAUSE=` set `1|0` : 1 will pause, 0 will not pause.
-- `_NEWLINE` (default : 1) compile with `-D _NEWLINE=` set `1|0` : 1 will print newlines, 0 will give more compact output.
-- `_FD` (default : 2) compile with `-D _FD=` sets the File Descriptor on which you want to output.
-- `_NO_COLORS` compile with `-D _NO_COLORS` to remove all colors.
-- `_VAR_WIDTH` (default : 2) compile with `-D _VAR_WIDTH=` to set the field width of printf for variables
-- `_MSG_WIDTH` (default : 2) compile with `-D _MSG_WIDTH=` to set the field width of printf for messages
-
+## Parameters
+Name           |How           | Compiler Option    | Description                                                                 | Default value
+-------------- | ------------ | ------------------ | --------------------------------------------------------------------------- | -------------
+`_BREAK_PAUSE` | compile with | `-D _BREAK_PAUSE=` | set `1`or `0` : `1` will pause, `0` will not pause.                         | 1
+`_NEWLINE`     | compile with | `-D _NEWLINE=`     | set `1`or `0` : `1` will print newlines, `0` will give more compact output. | 1
+`_FD`          | compile with | `-D _FD=`          | sets the File Descriptor on which you want to output.                       | 2
+`_VAR_WIDTH`   | compile with | `-D _VAR_WIDTH=`   | to set the field width of printf for variables                              | 8
+`_MSG_WIDTH`   | compile with | `-D _MSG_WIDTH=`   | to set the field width of printf for messages                               | 2
+`_NO_COLORS`   | compile with |`-D _NO_COLORS`     | to remove all colors.                                                       | not defined by default
 # Macros
-Automatic Macros
-- `BR` : BreakPoint : will pause the execution using libc `getchar()` so you can just press enter in the terminal to continue your program, you can `-D BREAK_PAUSE=0` so it does not pause your program.
-- `BM(msg)` : Same as `BR` but with a custom message.
-- `DE(var)` : Will try to detect variable type, and print it accordingly, using `_Generic()`.
-- `DM(msg, var)` : same as `DE` but you can put a message.
-
-Static typed (in case `_Generic()` fails)
-- `D_INT(var)` : (int) Print variable and its value.
-- `D_LINT(var)` : (long) Print variable and its value.
-- `D_STR(var)` : (String) Print variable and its value.
-- `D_PTR(var)` : (Pointer) Print variable and its value.
+## Automatic Macros
+Macro Name     | Mnemonic Name            | Decritpion
+-------------- | ------------------------ | ----------
+`BR`           | BreakPoint               | will pause the execution using libc `getchar()` so you can just press enter in the terminal to continue your program, you can `-D BREAK_PAUSE=0` so it does not pause your program.
+`BM(msg)`      | Breakpoint Message       | Same as `BR` but with a custom message but without pause.
+`BMB(msg)`     | BreakPoint Message Break | Same as `BR` but with a custom message, with pause.
+`DE(var)`      | Debug Variable           | Will try to detect variable type, and print it accordingly, using `_Generic()`.
+`DM(msg, var)` | Debug Message            | same as `DE` but calls `BM(msg)` before.
+`DB(...)`      | Debug Builder            | Allows to print a number of variables on one single line, and 'build' a debug line
+`DBM(msg, ...)`| Debug Builder Message    | Same as `DB(...)` but calls `BM(msg)` before.
+## Static Macros
+In case `_Generic()` fails to determine the correct type.
+Macro Name | Function
+:--------- | :-------
+`D_INT(var)` | (int) Print variable and its value.
+`D_LINT(var)` | (long) Print variable and its value.
+`D_DOUB(var)` | (double) Print variable and its value.
+`D_STR(var)` | (string) Print variable and its value.
+`D_CHR(var)` | (char) Print variable and its value.
+`D_PTR(var)` | (pointer) Print variable and its value.
 
 To debug memory manipulation with strings
-- `D_STR_DETAILS(str)` : Prints a string in details, printing control chars by their numeric value in the ascii table.
-- `D_STR_DETAILS_LEN(str, len)` : Prints a string in details forcing the len, printing control chars by their numeric value in the ascii table.
+Macro Name | Function
+---------- | --------
+`D_STR_DETAILS(str)` | Prints a string in details, printing control chars by their numeric value in the ascii table.
+`D_STR_DETAILS_LEN(str, len)` | Prints a string in details forcing the len, printing control chars by their numeric value in the ascii table.
 
 # Contributing
 Feel free to make a pull request to add your own macros.
@@ -39,3 +51,4 @@ Feel free to make a pull request to add your own macros.
 # Thanks
 - Thanks to [Jjosephi](https://github.com/FlavorlessQuark), for teaching me the existence of `_Generic()`.
 - Thanks to [Matthew-Dreemurr](https://github.com/Matthew-Dreemurr), for fixes.
+- Thanks to [Jonathan Heathcote](https://github.com/mossblaser) the autor of [C Pre-Processor Magic](http://jhnet.co.uk/articles/cpp_magic#iterators), for taking the time to write this, I took the liberty to backup this wonderful article in case this site goes down
--- debug_utils.h
+++ debug_utils.h
@@ -6,12 +6,12 @@
 /*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 18:21:34 by lorenuar          #+#    #+#             */
-/*   Updated: 2021/06/01 02:57:40 by lorenuar         ###   ########.fr       */
+/*   Updated: 2021/06/11 20:28:57 by lorenuar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-**	V2.2
+**	V3.0
 */
 
 #ifndef DEBUG_UTILS_H
@@ -22,7 +22,7 @@
 # include <string.h>
 
 # ifndef _VAR_WIDTH
-#  define _VAR_WIDTH 2
+#  define _VAR_WIDTH 8
 # endif
 
 # ifndef _MSG_WIDTH
@@ -63,10 +63,10 @@
 #  define _CL_RST ""
 # endif
 
-# define _print(fmt, ... ) dprintf( _FD, fmt, __VA_ARGS__ )
+# define _print(fmt, ... ) dprintf( _FD, fmt, ##__VA_ARGS__ );
 
-# define _BR(NEWLINE) _print("%s< %s:%d in %s() >" _CL_RST "%s",((NEWLINE) ? (_CL_BR) : (_CL_BR_FADE)), __FILE__, __LINE__, __FUNCTION__, (NEWLINE == 1) ? ("\n") : (" "))
-# define _BR_MSG(msg) _print( _CL_MSG "{%*s} " _CL_RST , _MSG_WIDTH,  #msg)
+# define _BR(NEWLINE) _print("%s< %s:%d in %s() >" _CL_RST "%s",((NEWLINE) ? (_CL_BR) : (_CL_BR_FADE)), __FILE__, __LINE__, __FUNCTION__, (NEWLINE == 1) ? ("\n") : (" "));
+# define _BR_MSG(msg) _print( _CL_MSG "{%*s} " _CL_RST , _MSG_WIDTH,  #msg);
 
 # if _BREAK_PAUSE == 1
 #  define BR _BR(1); getchar();
@@ -75,10 +75,12 @@
 # endif
 
 # if _BREAK_PAUSE == 1
-#  define BM(msg) _BR_MSG(msg); _BR(1); getchar();
+#  define BMB(msg) _BR_MSG(msg) _BR(1) getchar();
 # else
-#  define BM(msg) _BR_MSG(msg); _BR(1);
+#  define BMB(msg) _BR_MSG(msg) _BR(1)
 # endif
+
+# define BM(msg) _BR_MSG(msg) _BR(1)
 
 # define _DE_CONV_DEC(var) _Generic(((var)+0),	\
 	char					: "%*c", \
@@ -101,15 +103,16 @@
 
 # define _DE_AUTO(var) _print( _CL_VAR #var "%s", " = "); _print(_DE_CONV_DEC(var), _VAR_WIDTH, var); _print(_CL_RST "%s" , _DE_NL);
 
-# define DE(var) _BR(0); _DE_AUTO(var);
+# define DE(var) _BR(0) _DE_AUTO(var)
 
-# define DM(msg, var) _BR(0); _BR_MSG(msg); _DE_AUTO(var);
+# define DM(msg, var) _BR(0) _BR_MSG(msg) _DE_AUTO(var)
 
-# define D_INT(var)  _BR(0); _print(_CL_VAR  #var " : %d"     _DE_NL , var);
-# define D_LINT(var) _BR(0); _print( _CL_VAR #var " : %ld"    _DE_NL , var);
-# define D_DOUB(var) _BR(0); _print(_CL_VAR  #var " : %f"     _DE_NL , var);
-# define D_STR(var)  _BR(0); _print(_CL_VAR  #var " : \"%s\"" _DE_NL , var);
-# define D_PTR(var)  _BR(0); _print(_CL_VAR  #var " : <%p>"   _DE_NL , var);
+# define D_INT(var)  _BR(0) _print(_CL_VAR  #var " : %d"     _DE_NL , var);
+# define D_LINT(var) _BR(0) _print( _CL_VAR #var " : %ld"    _DE_NL , var);
+# define D_DOUB(var) _BR(0) _print(_CL_VAR  #var " : %f"     _DE_NL , var);
+# define D_STR(var)  _BR(0) _print(_CL_VAR  #var " : \"%s\"" _DE_NL , var);
+# define D_CHR(var)  _BR(0) _print(_CL_VAR  #var " : '%c'" _DE_NL , var);
+# define D_PTR(var)  _BR(0) _print(_CL_VAR  #var " : <%p>"   _DE_NL , var);
 
 # define D_STR_DETAILS(str) _BR(0); print_str_details(strlen(str), str, #str);
 
@@ -133,10 +136,77 @@ static inline void	print_str_details(size_t len, char *str, const char *name)
 		len--;
 		if (len > 0)
 		{
-			dprintf( _FD, "|");
+			_print("|");
 		}
 	}
-	dprintf( _FD, "] END =*=\n");
+	_print("] END =*=\n");
 }
+
+/* ************************************************************************** */
+/* ************************ RECURSIVE LINE BUILDER ************************** */
+/* ************************************************************************** */
+
+#define __FIRST(a, ...) a
+#define __SECOND(a, b, ...) b
+
+#define __EMPTY()
+
+#define __EVAL(...) __EVAL1024(__VA_ARGS__)
+#define __EVAL1024(...) __EVAL512(__EVAL512(__VA_ARGS__))
+#define __EVAL512(...) __EVAL256(__EVAL256(__VA_ARGS__))
+#define __EVAL256(...) __EVAL128(__EVAL128(__VA_ARGS__))
+#define __EVAL128(...) __EVAL64(__EVAL64(__VA_ARGS__))
+#define __EVAL64(...) __EVAL32(__EVAL32(__VA_ARGS__))
+#define __EVAL32(...) __EVAL16(__EVAL16(__VA_ARGS__))
+#define __EVAL16(...) __EVAL8(__EVAL8(__VA_ARGS__))
+#define __EVAL8(...) __EVAL4(__EVAL4(__VA_ARGS__))
+#define __EVAL4(...) __EVAL2(__EVAL2(__VA_ARGS__))
+#define __EVAL2(...) __EVAL1(__EVAL1(__VA_ARGS__))
+#define __EVAL1(...) __VA_ARGS__
+
+#define __DEFER1(m) m __EMPTY()
+#define __DEFER2(m) m __EMPTY __EMPTY()()
+#define __DEFER3(m) m __EMPTY __EMPTY __EMPTY()()()
+#define __DEFER4(m) m __EMPTY __EMPTY __EMPTY __EMPTY()()()()
+
+#define __IS_PROBE(...) __SECOND(__VA_ARGS__, 0)
+#define __PROBE() ~, 1
+
+#define CAT(a,b) a ## b
+
+#define __NOT(x) __IS_PROBE(CAT(_NOT_, x))
+#define ___NOT_0 __PROBE()
+
+#define __BOOL(x) __NOT(__NOT(x))
+
+#define __IF_ELSE(condition) ___IF_ELSE(__BOOL(condition))
+#define ___IF_ELSE(condition) CAT(_IF_, condition)
+
+#define _IF_1(...) __VA_ARGS__ _IF_1_ELSE
+#define _IF_0(...)             _IF_0_ELSE
+
+#define _IF_1_ELSE(...)
+#define _IF_0_ELSE(...) __VA_ARGS__
+
+#define __HAS_ARGS(...) __BOOL(__FIRST(_END_OF_ARGUMENTS_ __VA_ARGS__)())
+#define _END_OF_ARGUMENTS_() 0
+
+#define __MAP(m, first, ...)           \
+  m(first)                           \
+  __IF_ELSE(__HAS_ARGS(__VA_ARGS__))(    \
+    __DEFER2(_MAP)()(m, __VA_ARGS__)   \
+  )(                                 \
+    /* Do nothing, just terminate */ \
+  )
+#define _MAP() __MAP
+
+#define _PVAR(var) _print( _CL_VAR "- " #var " "); _print(_DE_CONV_DEC(var), -_VAR_WIDTH, var); _print(" -")
+
+#define _BUILD_LINE(...)  __EVAL(__MAP(_PVAR, ##__VA_ARGS__ ))
+
+
+#define DB(...) _BR(0) _BUILD_LINE(__VA_ARGS__) _print( _CL_RST "\n");
+
+#define DBM(msg, ...) _BR(0) _BR_MSG(msg) _BUILD_LINE(__VA_ARGS__) _print( _CL_RST "\n");
 
 #endif
